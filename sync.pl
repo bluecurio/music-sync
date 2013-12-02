3d7c05b0e01d25af120f887c39c8574f62e4b39b
DanielRenfro <bluecurio@gmail.com>
2013-12-01 22:55:37 -0500

    Big change, now mirrors directory structure

--- sync.pl
+++ sync.pl
@@ -3,31 +3,29 @@
 #
 #  sync.pl - a script by drenfro to sync music to an android phone w/ssh/rsync
 #
-#
-#
-# TODO
-# 
-# - add a help/usage() method for when the script is run with no options
-# - 
-#
-#
-#
 
 # Modules
 use warnings;
 use strict;
 use File::Copy;
-use File::Basename;
+use File::Spec;
+use Env qw/ HOME /;
 use Cwd;
 use Getopt::Long;
 use Pod::Usage;
 
+# Are you suffering from buffering?
+$| = 1;
+
 # Global variables
-our $help = 0;
-our $playlist = "";
-our $location = "";
-our $replace = 1;
-our $verbose = 0;
+our $help             = 0;
+our $playlist         = "";
+our $location         = "";
+our $include_playlist = 1;
+our $verbose          = 0;
+our $debug            = 0;
+our $replace          = 0;
+our $path_to_music    = $ENV{'HOME'} . '/Music/iTunes/iTunes Music/';
 
 sub verbose {
 	my $message = shift;
@@ -40,9 +38,12 @@ sub verbose {
 GetOptions(
 	'p|playlist=s' => \$playlist,
 	'l|location=s' => \$location,
-	'r|replace!'   => \$replace,
+	'i|include-playlist!'   => \$include_playlist,
+    'm|music=s' => \$path_to_music,
+	'r|replace!' => \$replace,
 	'v|verbose'    => \$verbose,
 	'h|?|help'     => \$help,
+	'd|debug'      => \$debug,
 );
 
 if ( $help ) {
@@ -55,6 +56,11 @@ if ( !$playlist ) {
 if ( !$location ) {
 	print STDERR  "No --location given\n\n";
 	pos2usage( 2 );
+}
+else {
+	if ( $location !~ /\/$/ ) {
+		$location .= '/';
+	}
 }
 if ( !-e $playlist ) {
 	print STDERR  "Bad path for --playlist\n\n";
@@ -72,49 +78,99 @@ if ( !-w $location ) {
 	print STDERR  "The --location path is not writable!\n\n";
 	exit( 1 );
 }
+if ( !-d $path_to_music ) {
+	print STDERR "The local iTunes music path (\"$path_to_music\") is invalid!\n\n";
+	exit( 1 );
+}
 
-# Get the name of the playlist - it will be a directory 
-my( $filename, $directories, $suffix ) = fileparse( $playlist );
-my $directoryName = $filename;
-$directoryName =~ s/\.[Mm]3[Uu]$//;
+# Debug means verbose, too.
+$verbose = 1 if $debug;
+verbose( "DEBUG MODE IS ON!\n" ) if $debug;
+verbose( "Playlist is: $playlist\n" );
+verbose( "Location is: $location\n" );
+verbose( "Music can be found in: $path_to_music\n\n" );
 
-verbose( "Directory name will be \"$directoryName\"\n" );
+# Handle the playlist file itself
+my $playlistsDiretory = $location . '/playlists/';
+my $playlistsDiretoryHandle;
+if ( $include_playlist ) {
 
-# Check if we want to completely blow away the old directory for this playlist
-my $directoryPath = $location . "/" . $directoryName;
-if ( $replace ) {
-	if ( -d $directoryPath ) {
-		verbose( "Removing $directoryPath for replacement\n" );
-		unlink( $directoryPath );
+	# Create a place to put our playlist
+	if ( ! -d $playlistsDiretory ) {
+		verbose( "mkdir( $playlistsDiretory )\n" );
+		if ( !-d $playlistsDiretory ) {
+			mkdir $playlistsDiretory or die "mkdir failed: $!" unless $debug;
+		}
+		else {
+			verbose( "Found $playlistsDiretory\n" );
+		}
 	}
-}
-else {
-	verbose( "Not replacing $directoryPath\n" );
+
+	# Open a filehandle for writing our playlist
+	my ( $root, $directories, $playlistFilename ) = File::Spec->splitpath( $playlist );
+	my $playlistPath = $playlistsDiretory . '/' . $playlistFilename;
+	verbose( "open( \$handle, '>', $playlistPath )\n" );
+	open( $playlistsDiretoryHandle, '>', $playlistPath ) or die "Could not open file for writing: $!";
 }
 
-# Create a directory for this playlist
-if ( ! -d $directoryPath ) {
-	verbose( "Creating $directoryPath\n" );
-	mkdir( $directoryPath );
-}
-
-# Do the actual copying
+# The loop
+my %directories_already_made;
 open( my $fh, '<', $playlist ) or die "Cannot read $playlist:$!";
 while (<$fh>) {
-	next if /^#/;
-	chomp;
-	copy( $_, $directoryPath ) or die "Copy failed: $!";
 
-	if ( $verbose ) {
-		verbose(
-			sprintf( "%s --> %s\n", $_, $directoryPath . "/" . $_ )
-		);
+	# Skip comments, blank lines, etc.
+	next if /^#/;
+	next if /^$/;
+	next if /^\s+$/;
+	chomp;
+
+	my $localPath = $_;
+	my $subDirectory = $localPath;
+	$subDirectory =~ s/^$path_to_music//;
+
+	my ( $root, $directories, $filename ) = File::Spec->splitpath( $subDirectory );
+	my @directories = ();
+	if ( $directories ) {
+		@directories = File::Spec->splitdir( $directories );
 	}
-	else {
-		print $_ . "\n";
+
+	if ( !$replace ) {
+		my $filePath = $location . $subDirectory;
+		print $filePath . "\n";
+		if ( -e $filePath ) {
+			verbose( "Already found $filePath\n" );  
+			next;
+		}
 	}
+
+	my $path = $location;
+	foreach my $directory ( @directories ) {
+		next unless $directory;
+		$path .=  "/$directory";
+		if ( !exists $directories_already_made{ $path } ) {
+			verbose( "Creating subdirectory...mkdir( $path )\n"  );
+			if ( !-d $path ) {
+				mkdir $path or die "mkdir failed: $!" unless $debug;
+			}
+			else {
+				verbose( "Found $path\n" );
+			}
+			$directories_already_made{ $path } = 1;
+		}
+	}
+
+	verbose( "copy( $localPath, $path )\n" );
+	copy( $localPath, $path ) or die "Copy failed: $!" unless $debug;
+
+	if ( $include_playlist ) {
+		print $playlistsDiretoryHandle "$subDirectory/$filename\n";
+	}
+
+	print $filename . "\n";
+	sleep 1 if $debug; # for realistic effect :)
 }
 
+close( $playlistsDiretoryHandle ) if $include_playlist;
 close( $fh );
 
 __END__
@@ -128,10 +184,13 @@ sync.pl - Copy files from a playlist to a location
 sync.pl [options] --playlist [path] --location [path]
 
  Options:
-   -p, --playlist     The path to a *.m3u playlist
-   -l, --location     Where to copy the files to
-   -r, --replace      If a directory exists with the name of the playlist,
-                       remove the directory first, essentially replacing it.
-		               (On by default. Use --noreplace to turn off.)
-   -h, --help         Show this help
-   -v, --verbose      Be more verbose
+   -p, --playlist             The path to a *.m3u playlist
+   -l, --location             Where to copy the files to
+   -i, --include-playlist     Copy the playlist file to the playlists/ directory. (On by default)
+   -m, --music                Path to the "iTunes Music" directory. Defaults to the one in the 
+                              running user's $HOME. ($HOME/Music/iTunes/iTunes Music/)
+   -r, --replace              Replace files, (Off by default to save resources.)
+
+   -h, --help                 Show this help
+   -v, --verbose              Be more verbose
+   -d, --debug                Fake copying/etc.
